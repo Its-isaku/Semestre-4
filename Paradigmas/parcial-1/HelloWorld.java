2287c17f5954ba2730842a461611d7fa0160c2db
Isaku <isaialmeraz2003@gmail.com>
2024-08-12 13:09:45 -0700

    dia_uno_de_uni

---
+++ Paradigmas/parcial-1/HelloWorld.java
@@ -0,0 +1,30 @@
+public class HelloWorld 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+        //---------------------------------------------
+
+        int a,b,c;
+
+        a = 0;
+        b = 0;
+        c = a + b;
+
+        System.out.println(c);
+
+        if ( c > 0)
+        {
+            System.out.println("El resultado es positivo");
+        }
+        else if (c < 0)
+        { 
+            System.out.println("El resultado es negativo");
+        }
+        else
+        {
+            System.out.println("El resultado es cero");
+        } 
+    }
+}
